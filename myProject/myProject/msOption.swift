a5aad9784b2affe9dc56ded58eb727722f416e85
student05 <student05@student03s-mac-5.local>
2017-03-07 03:21:07 -0800

    new form

---
+++ myProject/myProject/msOption.swift
@@ -0,0 +1,13 @@
+//
+//  msOption.swift
+//  myProject
+//
+//  Created by student05 on 07.03.17.
+//  Copyright © 2017 student05. All rights reserved.
+//
+
+import UIKit
+
+class msOption: NSObject {
+
+}
