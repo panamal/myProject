ce53bb84117f9fb71f9ab8b713d888db280a20f5
student05 <student05@student03s-mac-5.local>
2017-03-13 07:54:59 -0700

    Option +

--- myProject/myProject/optionVC.swift
+++ myProject/myProject/optionVC.swift
@@ -10,8 +10,62 @@ import UIKit
 
 class optionVC: UITableViewController {
 
+    @IBOutlet weak var txtServer: UITextField!
+    @IBOutlet weak var txtUserNick: UITextField!
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtUserPhone: UITextField!
+    @IBOutlet weak var imgUserAvatar: UIImageView!
+    @IBOutlet weak var btnCheckUser: UIButton!
+    @IBOutlet weak var btnServConn: UIButton!
+    
+    @IBAction func ifServerEdit(_ sender: UITextField) {
+        btnServConn.setTitleColor(UIColor.blue, for: .normal)
+        btnServConn.setTitle("Apply", for: UIControlState.normal)
+        btnServConn.tag = 0
+        btnCheckUser.setTitleColor(UIColor.blue, for: .normal)
+        btnCheckUser.setTitle("Check", for: UIControlState.normal)
+        btnCheckUser.tag = 0    }
+    
+    @IBAction func ifUserEdit(_ sender: UITextField) {
+        btnCheckUser.setTitleColor(UIColor.blue, for: .normal)
+        btnCheckUser.setTitle("Check", for: UIControlState.normal)
+        btnCheckUser.tag = 0
+    }
+    
+    @IBAction func btnServConnType(_ sender: UIButton) {
+        //********************
+        //connection to server
+        //********************
+        if btnServConn.tag == 0 {
+            btnServConn.tag = 1
+            btnServConn.setTitleColor(UIColor.green, for: .normal)
+            btnServConn.setTitle("Ok!", for: .normal)
+        }
+    }
+    
+    @IBAction func btnCheckUserType(_ sender: UIButton) {
+        //********************
+        //connection to server and check user nickname
+        //********************
+        if btnServConn.tag == 1 {
+            if btnCheckUser.tag == 0 {
+                btnCheckUser.tag = 1
+                btnCheckUser.setTitleColor(UIColor.red, for: .normal)
+                btnCheckUser.setTitle("Save", for: UIControlState.normal)
+            } else if btnCheckUser.tag == 1 {
+                btnCheckUser.tag = 2
+                btnCheckUser.setTitleColor(UIColor.green, for: .normal)
+                btnCheckUser.setTitle("Ok!", for: .normal)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        btnCheckUser.setTitleColor(UIColor.blue, for: .normal)
+        btnCheckUser.setTitle("Check", for: UIControlState.normal)
+        btnServConn.setTitleColor(UIColor.blue, for: .normal)
+        btnServConn.setTitle("Apply", for: UIControlState.normal)
         // Do any additional setup after loading the view.
     }
 
